d6f612036f490e77fc2052a22696100202362913
SpideErik <erikozh@gmail.com>
2026-09-10 20:45:04 +0300

    feat(hw-01): add homework1

---
+++ homework_01/src/exp3.py
@@ -0,0 +1,9 @@
+print('| a | b | c | r |')
+print('|--|--|--|--|')
+for a in range(2):
+    for b in range(2):
+        for c in range(2):
+            r = (a & b) | ~c
+            # нам нужен только младший бит (потому что ~ инвертирует все биты)
+            r &= 1
+            print(f'| {a} | {b} | {c} | {r} |')
